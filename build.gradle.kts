8a41fca4fdbadc4b13385e0885d29d4ee1a8157c
Ezra e. k. Cooper <ezra.cooper@ed.ac.uk>
2026-05-29 19:25:39 +0100

    Turn off "new exceptions" implementation in hopes of it working with wasmtime.

--- build.gradle.kts
+++ build.gradle.kts
@@ -184,3 +184,7 @@ tasks.withType<NodeJsExec>().matching { it.name.contains("wasmWasi", ignoreCase 
         )
     }
 }
+
+ tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile>().matching { it.name.contains("wasmWasi", ignoreCase = true) }.configureEach {
+    compilerOptions.freeCompilerArgs.addAll(listOf("-Xwasm-use-new-exception-proposal=false"))
+}
