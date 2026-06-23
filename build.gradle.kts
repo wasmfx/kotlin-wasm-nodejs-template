689385eb0aa5ddfa279175c5b001ee680fdf5033
Ezra e. k. Cooper <ezra.cooper@ed.ac.uk>
2026-06-23 13:15:01 +0100

    Enable stack-switching proposal for compilation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,9 +50,9 @@ apply<BinaryenPlugin>()
 the<BinaryenRootEnvSpec>().version.set(libs.versions.binaryen.get())
 
 // Uncomment to turn on stack-switching support
-//tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile>().configureEach {
-//    compilerOptions.freeCompilerArgs.addAll(listOf("-Xwasm-use-stack-switching-proposal"))
-//}
+tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile>().configureEach {
+    compilerOptions.freeCompilerArgs.addAll(listOf("-Xwasm-use-stack-switching-proposal"))
+}
 
 tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile>().matching { it.name.contains("wasmWasi", ignoreCase = true) }.configureEach {
     compilerOptions.freeCompilerArgs.addAll(listOf("-Xwasm-use-new-exception-proposal=false"))
